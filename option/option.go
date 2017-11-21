2eabc8ea826dc85b381255784f4bfc2243f082d9
realityone <realityone@me.com>
2017-11-22 02:44:44 +0800

    optional should not be a pointer

--- option/option.go
+++ option/option.go
@@ -11,81 +11,70 @@ var (
 type DefaultFunc = func() interface{}
 type MapFunc = func(interface{}) interface{}
 
-type Optional interface {
-	IsSome() bool
-	IsNone() bool
-	Unwrap() interface{}
-	UnwrapOr(interface{}) interface{}
-	UnwrapOrElse(DefaultFunc) interface{}
-	Map(MapFunc) Optional
-	MapOr(interface{}, MapFunc) interface{}
-	MapOrElse(DefaultFunc, MapFunc) interface{}
-}
-
 type Option struct {
 	none bool
 	some interface{}
 }
 
-func New(none bool, some interface{}) *Option {
-	return &Option{
+func New(none bool, some interface{}) Option {
+	return Option{
 		none: none,
 		some: some,
 	}
 }
 
-func Some(value interface{}) *Option {
+func Some(value interface{}) Option {
 	return New(false, value)
 }
 
-func None() *Option {
+func None() Option {
 	return New(true, nil)
 }
 
-func (o *Option) IsSome() bool {
+func (o Option) IsSome() bool {
 	return !o.none
 }
 
-func (o *Option) IsNone() bool {
+func (o Option) IsNone() bool {
 	return o.none
 }
 
-func (o *Option) Unwrap() interface{} {
+func (o Option) Unwrap() interface{} {
 	if o.none {
 		panic(ErrUnwrapNone)
 	}
 	return o.some
 }
 
-func (o *Option) UnwrapOr(or interface{}) interface{} {
+func (o Option) UnwrapOr(or interface{}) interface{} {
 	if o.none {
 		return or
 	}
 	return o.some
 }
 
-func (o *Option) UnwrapOrElse(f DefaultFunc) interface{} {
+func (o Option) UnwrapOrElse(f DefaultFunc) interface{} {
 	if o.none {
 		return f()
 	}
 	return o.some
 }
 
-func (o *Option) Map(f MapFunc) Optional {
+func (o Option) Map(f MapFunc) Option {
 	if o.none {
 		return None()
 	}
 	return Some(f(o.some))
 }
 
-func (o *Option) MapOr(u interface{}, f MapFunc) interface{} {
+func (o Option) MapOr(u interface{}, f MapFunc) interface{} {
 	if o.none {
 		return u
 	}
 	return f(o.some)
 }
 
-func (o *Option) MapOrElse(d DefaultFunc, f MapFunc) interface{} {
+func (o Option) MapOrElse(d DefaultFunc, f MapFunc) interface{} {
 	if o.none {
 		return d()
 	}
